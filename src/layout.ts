150ef240bb230c41a7918c570290b05dd13d7a1d
Isabel Paredes <iparedes314@gmail.com>
2022-07-01 11:24:21 +0200

    Clean up

--- src/layout.ts
+++ src/layout.ts
@@ -66,79 +66,52 @@ export class URDFLayout extends PanelLayout {
   }
 
   setURDF(data: string): void {
-    //console.debug("Data:", data);
-    const ros = new ROSLIB.Ros();
-    //ros.connect("ws://localhost:9090");
-
-    
 
     // Load robot model
-    if (this._robotModel == null) {
-      console.log('Robot is null');
-    } else {
-      console.log('NOT NULL');
-      // Robot is the first child of the group
-      // const oldRobot = this._viewer.scene.getObjectByName(
-      //   this._robotModel.object.name
-      //   );
-      // oldRobot.parent.remove( oldRobot );
+    if (this._robotModel !== null) {
+      // Remove old robot model from visualization
+      // Viewer -> Scene -> Group -> Robot Model
       this._robotModel.object.parent.remove( this._robotModel.object );
+    } 
 
-
-      // this._viewer.scene.children[0].remove(this._robotModel.object);
-    }
-    
-    // const robotModel = new Amphion.RobotModel(ros, 'robot_description');
-    
     // https://github.com/RoboStack/amphion/blob/879045327e879d0bb6fe2c8eac54664de46ef675/src/core/urdf.ts#L46
-    // robotModel.loadURDF(data, robotModel.onComplete, {});
+    const ros = new ROSLIB.Ros();
     this._robotModel = new Amphion.RobotModel(ros, 'robot_description');
     this._robotModel.loadURDF(data, this._robotModel.onComplete, {});
-
-    this._robotModel.object.getObjectByName('base_to_shoulder_joint').setAngle(2);
-
-    // window['robot'] = this._robotModel;
-
     this._viewer.addVisualization(this._robotModel);
-
-    console.log(this._robotModel);
-  
-
   }
 
   /**
-   * Handle `update-request` messages sent to the widget.
+   * Handle `update-request` messages sent to the widget
    */
   protected onUpdateRequest(msg: Message): void {
     this._resizeWorkspace();
   }
 
   /**
-   * Handle `resize-request` messages sent to the widget.
+   * Handle `resize-request` messages sent to the widget
    */
   protected onResize(msg: Message): void {
     this._resizeWorkspace();
   }
 
   /**
-   * Handle `fit-request` messages sent to the widget.
+   * Handle `fit-request` messages sent to the widget
    */
   protected onFitRequest(msg: Message): void {
     this._resizeWorkspace();
   }
 
   /**
-   * Handle `after-attach` messages sent to the widget.
+   * Handle `after-attach` messages sent to the widget
    */
   protected onAfterAttach(msg: Message): void {
-    //inject URDF viewer with appropiate JupyterLab theme.
-    //inject Amphion.
+    // Inject Amphion
     this._viewer = new Amphion.Viewer3d();
     this._viewer.setContainer(this._host);
   }
 
   private _resizeWorkspace(): void {
-    //Resize logic.
     const rect = this.parent?.node.getBoundingClientRect();
     this._host.style.height = rect?.height + 'px';
   }
