841f0dc22c763a1b16c0665cfc7bdcf7887045ca
Isabel Paredes <iparedes314@gmail.com>
2022-07-06 15:04:57 +0200

    Modify test

--- ui-tests/tests/jupyterlab_urdf.spec.ts
+++ ui-tests/tests/jupyterlab_urdf.spec.ts
@@ -16,6 +16,6 @@ test('should emit an activation console message', async ({ page }) => {
   await page.goto();
 
   expect(
-    logs.filter(s => s === 'JupyterLab extension jupyterlab_urdf is activated!')
+    logs.filter(s => s === 'JupyterLab extension URDF is activated!')
   ).toHaveLength(1);
 });
