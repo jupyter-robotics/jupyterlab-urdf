b3a8168cbf32eaf4d8c7f23de47968ef23f51870
Isabel Paredes <isabel.paredes@quantstack.net>
2023-07-27 14:26:47 +0200

    Simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   experiments: {
-    asyncWebAssembly: true,
-    buildHttp: true,
-    layers: true,
-    lazyCompilation: true,
-    outputModule: true,
-    syncWebAssembly: true,
-    topLevelAwait: true
+    asyncWebAssembly: true
   }
 };
