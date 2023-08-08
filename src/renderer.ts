9fe614f32753870a2cdc734f77728e7cee001879
Isabel Paredes <isabel.paredes@quantstack.net>
2023-08-08 16:37:37 +0200

    Replace renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -15,18 +15,19 @@ export class URDFRenderer extends THREE.WebGLRenderer {
     private _scene: THREE.Scene;
     private _camera: THREE.PerspectiveCamera;
     private _controls: OrbitControls;
-    private _colorSky: THREE.Color;
-    private _colorGround: THREE.Color;
+    private _colorSky = new THREE.Color();
+    private _colorGround = new THREE.Color();
+    private _robotIndex: number = -1;
 
     constructor (
         colorSky = new THREE.Color(0x263238),
-        colorGround = new THREE.Color(0x364248)
+        colorGround = new THREE.Color(0x263238)
     ) {
         super({ antialias: true });
 
         this._colorSky = colorSky;
         this._colorGround = colorGround;
-
+        
         this.setClearColor(0xffffff);
         this.setClearAlpha(0);
         this.outputColorSpace = THREE.SRGBColorSpace;
@@ -142,6 +143,16 @@ export class URDFRenderer extends THREE.WebGLRenderer {
     setGridHeight(height: number = 0): void {
         const gridIndex = this._scene.children.map(i => i.type).indexOf('GridHelper');
         this._scene.children[gridIndex].position.y = height;
+        this.redraw();
+    }
+
+    setRobot(robot: any): void {
+        if (this._robotIndex < 0) {
+            this._scene.add(robot);
+            this._robotIndex = this._scene.children.map(i => i.name).indexOf(robot.name);
+        } else {
+            this._scene.children[this._robotIndex] = robot;
+        }
         this.redraw();
     }
 
