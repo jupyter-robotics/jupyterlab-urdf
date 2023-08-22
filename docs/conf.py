8d2678f7ed868f5127ed39fe5e4d819b9785bb50
Isabel Paredes <isabel.paredes@quantstack.net>
2023-08-22 18:16:48 +0200

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,8 @@ master_doc = 'index'
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_favicon = 'favicon.ico'
+html_logo = 'logo.svg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
