7d31fbfe682c971e1b139d3297ca0b59a707300f
Isabel Paredes <isabel.paredes@quantstack.net>
2023-08-24 15:12:45 +0200

    Disable ipynb docs source binding

--- docs/conf.py
+++ docs/conf.py
@@ -63,9 +63,10 @@ html_logo = 'logo.svg'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # JupyterLite
 jupyterlite_config = "jupyterlite_config.json"
 jupyterlite_contents = ["robot.urdf"]
 jupyterlite_dir = "."
+jupyterlite_bind_ipynb_suffix = False
