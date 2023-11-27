107b7ed297c06fc8d6e83988a09d5ae01e8f5cfb
Isabel Paredes <isabel.paredes@quantstack.net>
2023-11-27 20:09:41 +0100

    Update version

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2023, Isabel Paredes'
 author = 'Isabel Paredes'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.3.2'
 
 
 # -- General configuration ---------------------------------------------------
